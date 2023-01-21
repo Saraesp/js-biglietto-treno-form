e37d9a9418f9ccf45d854d41c73ed140cd6e93c5
Saraesp <saraesposito145@gmail.com>
2023-01-21 21:50:39 +0100

    Creazione style biglietto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
-const button_prezzo = document.getElementById('button');
-prezzo.addEventListener('click', function(){
-    let nome_cognome = document.getElementById('nome_user').value;
-    let eta = document.getElementById('eta_user').value;
-    let chilometri = document.getElementById('input_km').value;
+const button_prezzo = document.getElementById('calc_prezzo');
+let nome_cognome;
+let eta;
+let chilometri;
+let prezzo; 
+let offerta;
 
-    let prezzo = 0.21 * chilometri;
+button_prezzo.addEventListener('click', function(){
+    nome_cognome = document.getElementById('nome_user').value;
+    eta = document.getElementById('eta_user').value;
+    chilometri = document.getElementById('input_km').value;
 
-    if(eta == 'minorenne'){
-        prezzo = 0.21 * chilometri * 0.8;
-    }else if (eta == 'over'){
-        prezzo = 0.21 * chilometri * 0.6;
+    prezzo;
+
+    if(eta == 1){
+        prezzo = (0.21 * chilometri * 0.8).toFixed(2);
+    }else if (eta == 3){
+        prezzo = (0.21 * chilometri * 0.6).toFixed(2);
     }else{
-        prezzo = 0.21 * chilometri;
+        prezzo = (0.21 * chilometri).toFixed(2);
     }
+
+    document.getElementById('inserisci_nome').innerText = nome_cognome;
+    document.getElementById('inserisci_prezzo').innerText = `${prezzo} €`;
+
+    offerta;
+
+    if(eta == 1){
+       offerta = 'Sconto MINORENNI'
+    }else if (eta == 3){
+        offerta = 'Sconto OVER'
+    }else{
+        offerta = 'Sconto STANDARD'
+    }
+
+    document.getElementById('inserisci_offerta').innerText = `${offerta}`;
+
+    let randNum = Math.floor(Math.random()*10);
+    document.getElementById('inserisci_cabina').innerText = `${randNum}`;
 });
